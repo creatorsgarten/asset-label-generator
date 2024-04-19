4da3ab59c0ecce5cee019417a3d19aec9a047d30
dtinth on MBP M1 <dtinth@spacet.me>
2024-04-19 18:38:28 +0700

    initial commit

---
+++ src/FreeType.ts
@@ -0,0 +1,132 @@
+import FreeTypeInit, { FT_GlyphSlotRec } from "freetype-wasm/dist/freetype.js";
+import wasmUrl from "freetype-wasm/dist/freetype.wasm?url";
+
+async function initializeFreeType() {
+  const FreeType = await FreeTypeInit({
+    locateFile: () => wasmUrl,
+  });
+  return FreeType;
+}
+
+export async function createFreeTypeRenderer() {
+  const FreeType = await initializeFreeType();
+  const loadFont = async (url: string, { forceAutoHint = true } = {}) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to load font");
+    }
+    const font = await response.arrayBuffer();
+    const [face] = FreeType.LoadFontFromBytes(new Uint8Array(font));
+    const withSize = (
+      size: number,
+      handleNewGlyph = (
+        code: number,
+        char: string,
+        glyph: FT_GlyphSlotRec
+      ) => {}
+    ) => {
+      const cache = new Map();
+      async function updateCache(str: string) {
+        FreeType.SetFont(face.family_name, face.style_name);
+        FreeType.SetCharmap(FreeType.FT_ENCODING_UNICODE);
+        FreeType.SetPixelSize(0, size);
+
+        // Get char codes without bitmaps
+        const codes = [];
+        for (const char of new Set(str)) {
+          const point = char.codePointAt(0);
+          if (!cache.has(char) && point !== undefined) {
+            codes.push(point);
+          }
+        }
+
+        // Populate missing bitmaps
+        const newGlyphs = FreeType.LoadGlyphs(
+          codes,
+          FreeType.FT_LOAD_RENDER |
+            FreeType.FT_LOAD_MONOCHROME |
+            FreeType.FT_LOAD_TARGET_MONO |
+            (forceAutoHint ? FreeType.FT_LOAD_FORCE_AUTOHINT : 0)
+        );
+        for (const [code, glyph] of newGlyphs) {
+          const char = String.fromCodePoint(code);
+          await handleNewGlyph(code, char, glyph);
+          cache.set(char, {
+            glyph,
+            bitmap: glyph.bitmap.imagedata
+              ? await createImageBitmap(glyph.bitmap.imagedata)
+              : null,
+          });
+        }
+      }
+
+      const draw = async (
+        ctx: CanvasRenderingContext2D,
+        str: string,
+        offsetx: number,
+        offsety: number,
+        { letterSpacing = 0 } = {}
+      ) => {
+        await updateCache(str);
+        let prev = null;
+        for (const char of str) {
+          const { glyph, bitmap } = cache.get(char) || {};
+          if (glyph) {
+            // Kerning
+            if (prev) {
+              const kerning = FreeType.GetKerning(
+                prev.glyph_index,
+                glyph.glyph_index,
+                0
+              );
+              offsetx += kerning.x >> 6;
+            }
+
+            if (bitmap) {
+              ctx.drawImage(
+                bitmap,
+                offsetx + glyph.bitmap_left,
+                offsety - glyph.bitmap_top
+              );
+            }
+
+            offsetx += glyph.advance.x >> 6;
+            offsetx += letterSpacing;
+            prev = glyph;
+          }
+        }
+      };
+
+      const measure = async (str: string, { letterSpacing = 0 } = {}) => {
+        await updateCache(str);
+        let width = 0;
+        let prev = null;
+        for (const char of str) {
+          const { glyph } = cache.get(char) || {};
+          if (glyph) {
+            if (prev) {
+              const kerning = FreeType.GetKerning(
+                prev.glyph_index,
+                glyph.glyph_index,
+                0
+              );
+              width += kerning.x >> 6;
+              width += letterSpacing;
+            }
+            width += glyph.advance.x >> 6;
+            prev = glyph;
+          }
+        }
+        return width;
+      };
+
+      const getGlyph = async (char: string) => {
+        await updateCache(char);
+        return cache.get(char);
+      };
+      return { draw, measure, getGlyph };
+    };
+    return { withSize };
+  };
+  return { loadFont };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import jetbrainsMonoUrl from "@fontsource/jetbrains-mono/files/jetbrains-mono-latin-600-normal.woff2?url";
+import { createFreeTypeRenderer } from "./FreeType";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function main() {
+  const renderer = await createFreeTypeRenderer();
+  const jetbrainsMono = await renderer.loadFont(jetbrainsMonoUrl);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+  const ctx = canvas.getContext("2d")!;
+  canvas.width = 256;
+  canvas.height = 112;
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+
+  await jetbrainsMono.withSize(20).draw(ctx, "Creatorsgarten", 10, 30);
+}
+
+main();
